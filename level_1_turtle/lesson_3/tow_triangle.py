c5feae63daf2a134b711e8845ccb95ebff0cb66b
JunkeyLau <QQ572204359@gmail.com>
2018-10-26 10:53:00 +0800

    添加第三课内容

---
+++ level_1_turtle/lesson_3/tow_triangle.py
@@ -0,0 +1,19 @@
+from turtle import * # 导入turtle模块
+screensize(300,300)
+pencolor("green")
+forward(100) # 画第一个三角形
+left(120)
+forward(100)
+left(120)
+forward(100)
+right(60)
+penup() # 提笔
+forward(100)
+pendown() # 放笔
+forward(100) # 画第二个三角形
+right(120)
+forward(100)
+right(120)
+forward(100)
+right(120)
+done()
