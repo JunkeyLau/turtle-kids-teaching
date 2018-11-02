195db27f8125305b629be37515caac7f55d7e8fc
JunkeyLau <QQ572204359@gmail.com>
2018-11-03 00:47:25 +0800

    修改课程内容

--- level_1_turtle/lesson_4/red_flag.py
+++ level_1_turtle/lesson_4/red_flag.py
@@ -1,16 +1,27 @@
-from turtle import * # 导入Turtle库
-screensize(1000,1000) # 设置一个300*300的画布
-penup()
-goto(-250,-125)
-pendown()
-color("red", "red")
-begin_fill()
-forward(500)
+from turtle import *  # 导入Turtle库
+screensize(500, 500)  # 设置一个500*500的画布
+speed(1)  # 设置画笔速度为1
+penup()  # 提笔
+goto(-152.5, 50)  # 将画笔向左移动152.5个单位
+pendown()  # 放笔
+color("red", "red")  # 设置画笔颜色为红色，填涂颜色为红色
+begin_fill()  # 开始填涂
+forward(300)  # 画红旗
 left(90)
-forward(250)
+forward(150)
 left(90)
-forward(500)
+forward(300)
 left(90)
-forward(250)
-left(90)
-end_fill()
+forward(150)
+end_fill()  # 结束填涂
+color("brown", "brown")  # 设置画笔颜色为棕色，填涂颜色为棕色
+begin_fill()  # 开始填涂
+forward(250)  # 画旗杆
+right(90)
+forward(5)
+right(90)
+forward(400)
+right(90)
+forward(5)
+end_fill()  # 结束填涂
+done()  # 结束画图
